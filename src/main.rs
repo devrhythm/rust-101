8fa9934966e202248cc17da53f53165e88fbe7e8
SUMETH K <sumeth_dev@live.com>
2025-01-19 21:05:20 +0700

    added sample data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let x2: i32 = 5;
+    let y = 0.5;
+    let y2: f64 = 0.5;
+
+    let z = x + y as i32;
+
+    let msg = String::from("Hello, world!");
+    let msg2 = "Hello, world!".to_string();
+    let msg3 = "Hello, world!";
+    let msg4  = format!("{}, x:{}, y:{}, z:{}", "hello", x ,y, z);
+
+    println!("{}", msg4);
 }
